63bb39460fdc1edb926abd07aa1c6ea62b4e7685
ShRi Shivam <dev.shrishivam@gmail.com>
2024-05-25 22:43:16 +0530

    update 01.py code file

--- 01.py
+++ 01.py
@@ -4,16 +4,17 @@ def si(p,r,t):
     return (p*r*t)/100
 
 def ci(p,r,t,n):
-    return p*(1+(r/n))**(n*t)
+    amount = p*(1+(r/(n*100)))**(n*t)
+    return amount - p
 
-p = int(input('Enter the principal amount: '))
-r = int(input('Enter the rate of interest: '))
-t = int(input('Enter the time period: '))
-n = int(input("Enter the number of times interest is compounded per year: "))
+p = float(input('Enter the principal amount: '))
+r = float(input('Enter the rate of interest (in %): '))
+t = float(input('Enter the time period (in years): '))
+n = float(input("Enter the number of times interest is compounded per year: "))
 
 
-simple_interest = si(p,r,t)
-compound_interest = ci(p,r,t,n)
+simple_interest = round(si(p,r,t),2)
+compound_interest = round(ci(p,r,t,n),2)
 
 print(f'Simple interest = {simple_interest}')
 print(f'Compound interest = {compound_interest}')
