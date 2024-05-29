d7decf779627e8735c838276c61c220f29e030c1
ShRi Shivam <dev.shrishivam@gmail.com>
2024-05-29 11:20:53 +0530

    define module and add 06.py code file

---
+++ 06.py
@@ -0,0 +1,7 @@
+# Write a python program to define a module to find Fibonacci Numbers and import the module to another program.
+
+# This is the main program
+import fibonacci  # importing the module fron fibonacci.py
+
+n = int(input('Enter the number of terms: '))   
+print(fibonacci.fib(n))
---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+# This is the module for 06_py
+# Fibonacci module
+def fib(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else :
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < n:
+            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        return fib_sequence
